168dc390e6ac0e540fd3a37bf2f2d42a2948b83c
Will Barton <william.barton@cfpb.gov>
2017-03-29 17:39:53 -0400

    Remove accidental changes

--- flags/apps.py
+++ flags/apps.py
@@ -2,12 +2,6 @@ from django.apps import AppConfig
 
 from flags.settings import add_flags_from_sources
 
-# Private set of all global flags from sources in FLAG_SOURCES.
-# Possible importable sources of global flags. This is expected to be a list
-# of Python module with flags specified with variable assignment, e.g.
-# MY_FLAG = True
-__SOURCED_FLAGS = {}
-
 
 class WagtailFlagsConfig(AppConfig):
     name = 'flags'
