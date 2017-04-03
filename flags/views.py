abc955416d48630b4fc83f13f534901fd6f712ef
Will Barton <william.barton@cfpb.gov>
2017-04-03 12:29:02 -0400

    Remove unused import

--- flags/views.py
+++ flags/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.forms import modelformset_factory
 from django.shortcuts import get_object_or_404
