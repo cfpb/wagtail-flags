0e033e4c343ed0c288f789da2b1e5c06fe4bc0a3
Andy Chosak <andy.chosak@cfpb.gov>
2019-02-22 15:23:48 -0500

    Autoregister site condition on app ready
    This change adds a custom app config for the wagtailflags package,
    including a ready() method that ensures that the 'site' custom condition
    gets registered on Django startup.

---
+++ wagtailflags/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class WagtailFlagsAppConfig(AppConfig):
+    name = 'wagtailflags'
+    verbose_name = 'Wagtail Flags'
+
+    def ready(self):
+        # Import custom conditions to ensure that they get registered.
+        import wagtailflags.conditions  # noqa
