cf1c22ab6a545a98c708cdeea49998aff5dbc588
Will Barton <william.barton@cfpb.gov>
2022-07-06 17:08:10 -0400

    Adopt setup.cfg in place of setup.py
    We don’t do anything other than set options in our existing setup.py, which is the purpose of setup.cfg. This change moves those to setup.cfg and removes those from setup.py.
    
    It also moves our flake8 and isort config to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,37 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-install_requires = [
-    "wagtail>=2.15,<4",
-    "django-flags>=4.2,<5.1",
-]
-
-testing_extras = ["coverage>=3.7.0"]
-
-setup(
-    name="wagtail-flags",
-    url="https://github.com/cfpb/wagtail-flags",
-    author="CFPB",
-    author_email="tech@cfpb.gov",
-    description="Feature flags for Wagtail sites",
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    license="CC0",
-    version="5.2.0",
-    include_package_data=True,
-    packages=find_packages(),
-    python_requires=">=3.8",
-    install_requires=install_requires,
-    extras_require={"testing": testing_extras},
-    classifiers=[
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Framework :: Wagtail",
-        "Framework :: Wagtail :: 2",
-        "Framework :: Wagtail :: 3",
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "License :: Public Domain",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
-)
+if __name__ == '__main__':
+    setup()
